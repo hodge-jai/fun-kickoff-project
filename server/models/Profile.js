9090ed091e6d40cab78642aec3018cfa2f732b80
Andrew Hodge <hodge.iaj@gmail.com>
2021-11-08 21:58:32 -0800

    added profile model to backend

---
+++ server/models/Profile.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const profileSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  availability: {
+    type: [Date],
+  },
+  photo: {
+    type: String,
+  }
+});
+
+module.exports = User = mongoose.model("profile", profileSchema);
